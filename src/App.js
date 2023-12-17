c2f626e55e58169cb84d6403965fb2e59c05d753
AmanAsharma070320 <asharma070320@gmail.com>
2023-12-17 21:41:13 +0530

    Day3 Homework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import Body from "./Components/Body";
+import Text from "./Components/Text";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    <div>
+      <Header />
+
+      <div className="flexprop" style={{
+        display: "flex",
+      }}>
+        <Navbar name="HOME" />
+        <Navbar name="ABOUT"/>
+        <Navbar name="SERVICES"/>
+        <Navbar name="FAQ"/>
+        <Navbar name="CONTACT"/>
+      </div>
+      <div class="image">
+      <img src="https://media.istockphoto.com/id/503219100/photo/people-in-a-park.jpg?s=612x612&w=0&k=20&c=VP0QT_MR_Cckke6jA30CX7c1G3gwwVcUifgcLfOY-IQ="></img>
+      </div>
+      <div class="main">
+        <Body />
+      </div>
+      <div class="text">
+        <Text />
+      </div>
+      <br /> <br />
+      <div class="footer">
+        <Footer />
+      </div>
+      <div class="footWidth">
+        <p>Powered by Website.com</p>
+      </div>
     </div>
+   
   );
 }
 
